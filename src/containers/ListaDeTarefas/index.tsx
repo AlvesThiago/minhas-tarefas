42a4f46a30931d469bdf224aaa1b2d03aa636ce8
Thiago Alves (ADMIN) <thiagodesign.27@gmail.com>
2025-02-28 06:05:05 -0300

    Card Tarefa

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,3 +1,19 @@
-const ListaDeTarefas = () => <h1>TEste</h1>
+import Tarefa from '../../components/FiltroCard/Tarefa'
+import * as S from './styles'
+
+const ListaDeTarefas = () => (
+  <S.Container>
+    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
+    <ul>
+      <li>
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </li>
+    </ul>
+  </S.Container>
+)
 
 export default ListaDeTarefas
