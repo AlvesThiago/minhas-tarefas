1e911376ff4ef2d85e5076185d842c7bbf04b50c
Thiago Alves (ADMIN) <thiagodesign.27@gmail.com>
2025-03-05 05:25:40 -0300

    adicionando funcionalidades nos botoes

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,17 +1,41 @@
 import Tarefa from '../../components/FiltroCard/Tarefa'
 import * as S from './styles'
 
+const tarefas = [
+  {
+    tituilo: 'Estudar JavaScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    tituilo: 'Estudar JavaScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    tituilo: 'Estudar JavaScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <S.Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.tituilo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.tituilo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </S.Container>
 )
